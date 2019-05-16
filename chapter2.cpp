ded9e0aeab752dcc7c9d3e27d3de5d933260e7da
fateful <vladislavvladislavovich@gmail.com>
2019-05-16 22:06:13 +0300

    Done chapter 2 Guess my number

---
+++ chapter2.cpp
@@ -0,0 +1,44 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+    bool guessed = false;
+    short luckyNumber, guess, tries = 0;
+    const short MAX_NUMBER = 100;
+
+    unsigned int seed = static_cast<unsigned int>(time(0));
+    srand(seed);
+
+    luckyNumber = rand() % MAX_NUMBER;
+
+    cout << "Welcome to Guess My Number!" << endl << "RULES: " << endl;
+    cout << "You need to guess a number from 0 to "<< MAX_NUMBER;
+    cout << " in as few attempts as possible." << endl;
+    cout << "After each attempt computer will tell you if the number is larger, ";
+    cout << "smaller or if your guessed it correctly." << endl;
+    cout << "Good Luck!" << endl;
+
+    while (!guessed)
+    {
+        tries++;
+        cout << endl << "Enter a guess ";
+        cin >> guess;
+
+        if (guess > luckyNumber)
+            cout << "Too high!";
+        else if (guess < luckyNumber)
+            cout << "Too low!";
+        else 
+        {
+            cout << "Congratulations!" << endl; 
+            cout << "You guessed it using "<< tries <<" guesses";
+            guessed = true;
+        }
+    }
+}
