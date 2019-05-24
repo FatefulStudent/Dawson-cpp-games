6438926b96a0b7ba9ac16e0d1a2ae3ea47211003
fateful <vladislavvladislavovich@gmail.com>
2019-05-25 00:26:07 +0300

    Mad lib submit

--- chapter5.cpp
+++ chapter5.cpp
@@ -1,12 +1,55 @@
+// Mad-Lib
+// Creates a story based on user input
 #include <iostream>
+#include <string>
 using namespace std;
+string askText(string prompt);
+int askNumber(string prompt);
+void tellStory(string name, string noun, int number, string bodyPart, 
+string verb);
 
-void foo(int a, int b, int c=0);
 int main()
 {
-    foo(10, 2);
+    cout << "Welcome to Mad Lib.\n\n";
+    cout << "Answer the following questions to help create a new story.\n";
+    string name = askText("Please enter a name: ");
+    string noun = askText("Please enter a plural noun: ");
+    int number = askNumber("Please enter a number: ");
+    string bodyPart = askText("Please enter a body part: ");
+    string verb = askText("Please enter a verb: ");
+    tellStory(name, noun, number, bodyPart, verb);
+    return 0;
 }
-void foo(int a, int b, int c)
+
+int askNumber(string prompt)
 {
-    cout << a << " " << b << " " << c;
+    int number;
+    cout << prompt;
+    cin >> number;
+    return number;
+}
+
+string askText(string prompt)
+{
+    string word;
+    cout << prompt;
+    cin >> word;
+    return word;
+}
+
+
+void tellStory(string name, string noun, int number, string bodyPart, 
+string verb)
+{
+    cout << "\nHere's your story:\n";
+    cout << "The famous explorer "<< name;
+    cout << " had nearly given up a life-long quest to find\n";
+    cout << "The Lost City of " << noun;
+    cout << " when one day, the " << noun << " found the explorer.\n";
+    cout << "Surrounded by " << number << " " << noun << ", a tear came to ";
+    cout << name << "'s "<< bodyPart << ".\n";
+    cout << "After all this time, the quest was finally over. ";
+    cout << "And then, the " << noun << "\n";
+    cout << "promptly devoured " << name << ". ";
+    cout << "The moral of the story? Be careful what you " << verb << " for.";
 }
