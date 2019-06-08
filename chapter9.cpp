09b8e056c26c0526f9ae53a680ca83fa3e204a47
fateful <vladislavvladislavovich@gmail.com>
2019-06-08 17:37:49 +0300

    heap and leaks

--- chapter9.cpp
+++ chapter9.cpp
@@ -1,44 +1,40 @@
 #include <iostream>
-#include <string>
 using namespace std;
-class Critter
-{
-    //make following global functions friends of the Critter class
-    friend void Peek(const Critter& aCritter);
-    friend ostream& operator<<(ostream& os, const Critter& aCritter);
-public:
-    Critter(const string& name = "");
-private:
-    string m_Name;
-};
-
-Critter::Critter(const string& name):
-m_Name(name)
-{}
-
-void Peek(const Critter& aCritter);
-
-ostream& operator<<(ostream& os, const Critter& aCritter);
+int* intOnHeap(); //returns an int on the heap
+void leak1(); //creates a memory leak
+void leak2(); //creates another memory leak
 
 int main()
 {
-    Critter crit("Poochie");
-    cout << "Calling Peek() to access crit’s private data member, m_Name: \n";
-    Peek(crit);
-    cout << "\nSending crit object to cout with the << operator:\n";
-    cout << crit;
+    int* pHeap = new int;
+    *pHeap = 10;
+    cout << "*pHeap: " << *pHeap << "\n\n";
+    int* pHeap2 = intOnHeap();
+    cout << "*pHeap2: " << *pHeap2 << "\n\n";
+    cout << "Freeing memory pointed to by pHeap.\n\n";
+    delete pHeap;
+    cout << "Freeing memory pointed to by pHeap2.\n\n";
+    delete pHeap2;
+    //get rid of dangling pointers
+    pHeap = 0;
+    pHeap2 = 0;
     return 0;
 }
-//global friend function which can access all of a Critter object’s members
-void Peek(const Critter& aCritter)
+
+int* intOnHeap()
 {
-    cout << aCritter.m_Name << endl;
+    int* pTemp = new int(20);
+    return pTemp;
 }
-//global friend function which can access all of Critter object’s members
-//overloads the << operator so you can send a Critter object to cout
-ostream& operator<<(ostream& os, const Critter& aCritter)
+
+void leak1()
 {
-    os << "Critter Object - ";
-    os << "m_Name: " << aCritter.m_Name;
-    return os;
+    int* drip1 = new int(30);
+}
+
+void leak2()
+{
+    int* drip2 = new int(50);
+    drip2 = new int(100);
+    delete drip2;
 }
